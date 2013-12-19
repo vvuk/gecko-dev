4273aefde45ca29c933fc7f5c84f782a4dc57372
Vladimir Vukicevic <vladimir@pobox.com>
2013-12-19 17:30:50 -0500

    SQUASH GLContextSkia.h missing include guard fixup

--- gfx/gl/GLContextSkia.h
+++ gfx/gl/GLContextSkia.h
@@ -3,6 +3,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#ifndef GLCONTEXTSKIA_H_
+#define GLCONTEXTSKIA_H_
+
 #include "mozilla/RefPtr.h"
 #include "skia/GrGLInterface.h"
 #include "skia/GrContext.h"
@@ -36,3 +39,6 @@ private:
 
 }
 }
+
+#endif
+
